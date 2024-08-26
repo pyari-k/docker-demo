7d0e53d329b033c3115e2d1fdb639bd46e6e85e9
Pyari Kumaran <pyari.kumaran@primarybid.com>
2024-08-26 17:39:34 +0200

    minor changes for kubernetes demo

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import read_csv
 import ping_google
 from datetime import date
+from datetime import datetime
 import sys
 print("Python version")
 print (sys.version)
@@ -8,3 +9,4 @@ print("\n date: {}".format(date.today()))
 print("\n import completed")
 read_csv.count_rows_in_csv()
 ping_google.ping_google()
+print(f"time of running the code - {datetime.now()}")
